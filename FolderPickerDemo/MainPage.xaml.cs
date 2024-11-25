bb9f2d688272e409ae49911bbfb5ad07ec21194c
BeepBeepBopBop <evan.carrere@gmail.com>
2024-11-25 10:19:33 +0100

    initial commit

---
+++ FolderPickerDemo/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Maui.Storage;
+
+namespace FolderPickerDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+               var result = await FolderPicker.Default.PickAsync();
+
+                if (result != null)
+                {
+                    this.result.Text = result.IsSuccessful ? "folder picked: " + result.Folder.ToString() : "no folder picked";
+                }
+            });
+        }
+    }
+}
